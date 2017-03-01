89f36e7fd55fc05b4840abb20c78d49462bb1c90
laiaaa0 <laiafreixas9@gmail.com>
2017-03-01 18:02:10 +0100

    Implementar successors
    Usar boardCopy(); i getLength()

--- AIMAexample-master/IA/ProbIA5/ProbIA5SuccesorFunction.java
+++ AIMAexample-master/IA/ProbIA5/ProbIA5SuccesorFunction.java
@@ -12,12 +12,20 @@ public class ProbIA5SuccesorFunction implements SuccessorFunction{
 
     public List getSuccessors(Object state){
         ArrayList retval = new ArrayList();
-        ProbIA5Board board = (ProbIA5Board) state;
+        IA.ProbIA5.ProbIA5Board board = (IA.ProbIA5.ProbIA5Board) state;
 
         // Some code here
         // (flip all the consecutive pairs of coins and generate new states
         // Add the states to retval as Succesor("flip i j, new_state)
         // new_state has to be a copy of state
+
+        for (int i=0; i<board.getLength()-1; ++i){
+            IA.ProbIA5.ProbIA5Board board_current = (IA.ProbIA5.ProbIA5Board) board.boardCopy();
+            board.flip_it(i);
+            int newState[] = board.boardCopy();
+            retval.add (newState);
+            board=board_current;
+        }
 
         return retval;
 
