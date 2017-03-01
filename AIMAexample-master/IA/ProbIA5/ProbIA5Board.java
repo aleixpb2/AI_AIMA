0e7b4f100292db0fbe7e3d7b393aab5bbc6919d8
aleixpb2 <aleixpb2@gmail.com>
2017-03-01 21:04:00 +0100

    Implemented Board functions

--- AIMAexample-master/IA/ProbIA5/ProbIA5Board.java
+++ AIMAexample-master/IA/ProbIA5/ProbIA5Board.java
@@ -12,8 +12,9 @@ public class ProbIA5Board {
         vector with the parity of the coins (we can assume 0 = heads, 1 = tails
      */
 
-    private int [] board;
+    private int [] board;    
     private static int [] solution;
+    private int N;
 
     /* Constructor */
     public ProbIA5Board(int []init, int[] goal) {
@@ -25,29 +26,48 @@ public class ProbIA5Board {
             board[i] = init[i];
             solution[i] = goal[i];
         }
+        N = board.length;
 
     }
 
     /* vvvvv TO COMPLETE vvvvv */
     public void flip_it(int i){
         // flip the coins i and i + 1
+        board[i] = 1-board[i];
+        board[(i+1)%N] = 1-board[(i+1)%N];
     }
 
     /* Heuristic function */
     public double heuristic(){
         // compute the number of coins out of place respect to solution
-        return 0;
+        double diff = 0;
+        for(int i = 0; i < N; ++i){
+             if(board[i] != solution[i])
+                 ++diff;
+         }
+        return diff;
     }
 
      /* Goal test */
      public boolean is_goal(){
          // compute if board = solution
-         return false;
+         for(int i = 0; i < N; ++i){
+             if(board[i] != solution[i])
+                 return false;
+         }
+         return true;
      }
 
      /* auxiliary functions */
 
      // Some functions will be needed for creating a copy of the state
+     public int[] boardCopy(){
+         return board.clone();
+     }
+     
+     public int getLength(){
+         return N;
+     }
 
     /* ^^^^^ TO COMPLETE ^^^^^ */
 
